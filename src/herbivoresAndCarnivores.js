647eac1479dfd3116019bc5a2b05584bda832aa4
LX-y-ka <alexandravirtualife@gmail.com>
2024-12-01 22:20:07 +0200

    solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,42 @@
 
 class Animal {
   // write your code here
+  static alive = [];
+
+  health = 100;
+
+  constructor(name) {
+    this.name = name;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
   // write your code here
+  hidden = false;
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  bite(herbivore) {
+    if (!(herbivore instanceof Herbivore)) {
+      return;
+    }
+
+    if (herbivore.hidden) {
+      return;
+    }
+
+    herbivore.health -= 50;
+
+    if (herbivore.health <= 0) {
+      Animal.alive = Animal.alive.filter((a) => a !== herbivore);
+    }
+  }
 }
 
 module.exports = {
